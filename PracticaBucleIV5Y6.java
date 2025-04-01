a997d4be6a07d842e0ab91e4975853ec72aa064d
Daniel Navas34 <danielnavas711900@digitechfp.com>
2025-04-01 18:27:46 +0200

    hola

---
+++ PracticaBucleIV5Y6.java
@@ -0,0 +1,27 @@
+
+
+public class PracticaBucleIV5Y6 {
+
+    public static void main(String[] args) {
+
+        //(Con bucle do while), imprimir los números del 1-100 
+        int num = 1;
+        do {
+            System.out.println(num);
+            num++;
+
+        } while (num <= 100);
+
+        
+        //(Con bucle while), imprimir los números del 1-100
+        int num1 =1;
+                
+        while(num1<=100){
+            System.out.println(num1);
+            num1++;
+        }
+        
+        
+        
+    }
+}
